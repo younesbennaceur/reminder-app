16f7a9c25d5e25720224a5c0d3554495c5076fce
younes BENNACEUR <younesbennaceur06@gmail.com>
2025-12-05 04:47:08 +0100

    fix doployement issue

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,29 @@
-console.log("Service Worker chargé...");
+// public/sw.js
 
-self.addEventListener('push', e => {
-  const data = e.data.json();
-  
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    vibrate: [100, 50, 100], 
-    data: {
-      dateOfArrival: Date.now(),
-      primaryKey: 1
-    }
-  });
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    const data = event.data.json();
+    
+    const options = {
+      body: data.body,
+      icon: data.icon || '/Logo.png',
+      badge: '/Logo.png', // Petite icône dans la barre de statut (Android)
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      }
+    };
+
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  }
 });
-
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
-  
+  // Ouvre l'application si on clique sur la notif
   event.waitUntil(
     self.clients.openWindow('/')
   );
